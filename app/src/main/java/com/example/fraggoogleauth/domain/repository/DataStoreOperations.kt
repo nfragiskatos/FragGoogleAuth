3dfe76dad8666f89853377fdd86c280a11caa7b4
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-25 16:28:10 -0700

    - created interface + impl for storing/retrieving from a data store

---
+++ app/src/main/java/com/example/fraggoogleauth/domain/repository/DataStoreOperations.kt
@@ -0,0 +1,8 @@
+package com.example.fraggoogleauth.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface DataStoreOperations {
+    suspend fun saveSignedInState(signedIn: Boolean)
+    fun readSignedInState(): Flow<Boolean>
+}
--- app/src/main/java/com/example/fraggoogleauth/util/Constants.kt
+++ app/src/main/java/com/example/fraggoogleauth/util/Constants.kt
@@ -2,4 +2,6 @@ package com.example.fraggoogleauth.util
 
 object Constants {
     const val BASE_URL = "http://10.0.2.2:8080" // android emulator local host
+
+    const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
 }
