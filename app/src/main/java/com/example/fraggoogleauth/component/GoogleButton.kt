a201a18de1aa85aebd1748903e18e0612f83dc7c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-24 17:00:58 -0700

    - created composable for sign in button

---
+++ app/src/main/java/com/example/fraggoogleauth/component/GoogleButton.kt
@@ -0,0 +1,89 @@
+package com.example.fraggoogleauth.component
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fraggoogleauth.R
+import com.example.fraggoogleauth.ui.theme.LoadingBlue
+import com.example.fraggoogleauth.ui.theme.Shapes
+
+@Composable
+fun GoogleButton(
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    primaryText: String = "Sign in with Google",
+    secondaryText: String = "Please wait...",
+    icon: Int = R.drawable.ic_google_logo,
+    shape: Shape = Shapes.medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = LoadingBlue,
+    onClick: () -> Unit
+) {
+    var buttonText by remember {
+        mutableStateOf(primaryText)
+    }
+
+    LaunchedEffect(key1 = isLoading) {
+        buttonText = if (isLoading) secondaryText else primaryText
+    }
+
+    Surface(
+        modifier = modifier.clickable(enabled = !isLoading)
+        {
+            onClick()
+        },
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(start = 12.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = "Google Icon",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = buttonText)
+            if (isLoading) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier.size(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun GoogleButtonPreview() {
+    GoogleButton {
+
+    }
+}
